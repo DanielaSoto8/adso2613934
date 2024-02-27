4f4126ad88af28321e285683053fc30e42e9b99c
DanielaSoto8 <danivalenciasoto@gmail.com>
2024-02-26 21:04:07 -0500

    Se realizo las funcionalidades y se inicia  de Visibility

--- 01-oop/01-introduction.php
+++ 01-oop/01-introduction.php
@@ -1,23 +1,149 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>01- Introduction</title>
+    <link rel="stylesheet" href="css/master.css">
+    <style>
+        section {
+            background-color: #0009;
+            border-radius: 10px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 1rem;
+            padding: 10px;
 
-$num1=54;
-$num2=32;
+            h2 {
+                margin: 0;
+            }
 
-echo "{$num1} * {$num2} = "  . ($num1*$num2);
-echo "<br>";
+            form {
+                border: 2px solid #fff6;
+                background-color: #fff3;
+                border-radius: 8px;
+                display: flex;
+                flex-direction: column;
+                padding: 10px;
+                width: 300px;
 
-$string = "hello";
+                label {
+                    display: flex;
+                    justify-content: space-between;
+                    gap: 1.4rem;
+                }
 
-echo "MD5{$string} = "  . md5($string);
+                output {
+                    font-size: 1.4rem;
+                }
 
-echo "<br>";
-echo " PASSWORD_HASH ({$string}) = ". password_hash($string,PASSWORD_DEFAULT);
-echo"<br>";
-$hash = password_hash ($string, PASSWORD_DEFAULT);
+                button {
+                    background-color: #994bde;
+                    border: 2px solid #fff6;
+                    border-radius: 8px;
+                    color: #fff9;
+                    cursor: pointer;
+                    font-size: 1rem;
+                    width: 300px;
+                    padding: 1rem;
+                }
 
-if (password_verify($string,$hash)) {
-    echo "Verified successul!";
-}
+                div.result {
+                    background-color: #0f09;
+                    border-radius: 8px;
+                    color: #fffc;
+                    margin-top: 1rem;
+                    font-size: 1rem;
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+                    justify-content: center;
+                    padding: 10px;
+                    width: 280px;
+                }
+        }
+    </style>
+</head>
+<body>
+    <nav class="controls">
+        <a href="index.html">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM231 127c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-71 71L376 232c13.3 0 24 10.7 24 24s-10.7 24-24 24l-182.1 0 71 71c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L119 273c-9.4-9.4-9.4-24.6 0-33.9L231 127z"/></svg>    
+        </a>
+    </nav>
+    <main>
+        <h1>01- Introduction</h1>
+        <section>
+        <?php
+            # Linear Programming
+            echo "<h2> Linear Programming </h2>";
+            $num1 = 54;
+            $num2 = 32;
 
+            echo "> {$num1} * {$num2} = " . ($num1 * $num2);
+            echo "<br>";
+            $string = "hello";
+            //echo " MD5({$string}) = " . md5($string);
+            //echo " PASSWORD_HASH({$string}) = " . password_hash($string, PASSWORD_DEFAULT);
+            $hash = password_hash($string, PASSWORD_DEFAULT);
 
-?>
+            if (password_verify($string, $hash)) {
+                echo "> Password Hash Verified Successful!";
+            }
+
+            # Structured Progamming
+            echo "<h2> Structured Progamming </h2>";
+
+            function adition($num1, $num2 = 7) {
+                return ($num1 + $num2);
+            } 
+
+            $rs = adition(34, 890);
+            echo "> The sum of 34 and 890 = " . $rs . "<br>";
+            $rs = adition(89);
+            echo  "> The sum of 89 and 7 = " . $rs;
+
+            # Object Oriented Programming 
+            echo "<h2> Object Oriented Programming </h2>";
+
+            class Adition {
+                public $num1;
+                public $num2;
+
+                public function getResult() {
+                    return ($this->num1 + $this->num2);
+                }
+            }
+
+            // $sum = new Adition;
+            // $sum->num1 = 1024;
+            // $sum->num2 = 512;
+            // echo "The sum of {$sum->num1} and {$sum->num2} = " . 
+            //     $sum->getResult();
+        ?>
+        <form action="" method="post">
+            <label>
+                <p>Number 1:</p>
+                <input type="range" name="n1" step="1" value="0" oninput="o1.value=this.value">
+                <output id="o1">0</output>
+            </label>
+            <label>
+                <p>Number 2:</p>
+                <input type="range" name="n2" step="1" value="0" oninput="o2.value=this.value">
+                <output id="o2">0</output>
+            </label>
+            <button> Calculate </button>
+                <?php
+                    if ($_POST) {
+                        $sum = new Adition;
+                        $sum->num1 = $_POST['n1'];
+                        $sum->num2 = $_POST['n2'];
+                        echo "<div class='result'>The sum of {$sum->num1} and {$sum->num2} = " . 
+                             $sum->getResult() . "</div>";
+                    }
+                ?>
+        </form>
+        </section>
+    </main>
+</body>
+</html>
---
+++ 01-oop/02-fundamentals.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>02- Fundamentals</title>
+    <link rel="stylesheet" href="css/master.css">
+    <style>
+        section {
+            background-color: #0009;
+            border-radius: 10px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 1rem;
+            padding: 10px;
+
+            h2 {
+                margin: 0;
+            } 
+
+            ul {
+                padding: 0;
+                margin: 0;
+            }
+
+            figure {
+                background-color: #fff3;
+                border: 2px solid #fff6;
+                border-radius: 8px;
+                font-size: 6rem;
+                margin: 0;
+            }
+
+            form {
+                display: flex;
+                justify-content: space-between;
+                gap: 1rem;
+                width: 100%;
+                button {
+                    border: 2px solid #fff6;
+                    background-color: #994bde;
+                    border-radius: 8px;
+                    color: #fff9;
+                    cursor: pointer;
+                    font-size: 1rem;
+                    width: 100px;
+                    padding: 0.6rem;
+                }
+            }
+        }
+    </style>
+</head>
+<body>
+    <nav class="controls">
+        <a href="index.html">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill="#ffffff" d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM231 127c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-71 71L376 232c13.3 0 24 10.7 24 24s-10.7 24-24 24l-182.1 0 71 71c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L119 273c-9.4-9.4-9.4-24.6 0-33.9L231 127z"/></svg>    
+        </a>
+    </nav>
+    <main>
+        <h1>02- Fundamentals</h1>
+        <section>
+            <?php
+                
+                class Runner {
+                    // Attributes
+                    public $name;
+                    public $age;
+                    public $number;
+
+                    // Methods
+                    public function __construct($name, $age, $number) {
+                        $this->name   = $name;
+                        $this->age    = $age;
+                        $this->number = $number;
+                    }
+
+                    public function run() {
+                        return '<img src="./corriendo" alt="">';
+                    }
+
+                    public function stop() {
+                        return "🧎🏻‍♂️";
+                    }
+
+                    public function jump() {
+                        return "🚶🏻‍♂️";
+                    }
+                }
+
+                $runner = new Runner('Usain Bolt', 35, 105);
+
+            ?>
+            <h2>Class Runner</h2>
+            <ul>
+                <li>Name:   <?=$runner->name ?></li>
+                <li>Age:    <?=$runner->age ?></li>
+                <li>Number: <?=$runner->number ?></li>
+            </ul>
+            <figure>
+                <?php
+                if ($_POST){
+                    if(isset($_POST['run'])){
+                        echo $runner->run();
+                        
+                    }
+                    elseif (isset($_POST['stop'])){
+                        echo $runner->stop();
+                    }
+                    else
+                    echo $runner->jump();
+
+                }
+                    
+                    
+                    
+                   
+                ?>
+            </figure>
+            <form action="" method="post">
+                <button> Run </button>
+                <button> Stop </button>
+                <button> Jump </button>
+            </form>
+        </section>
+    </main>
+</body>
+</html>
